eff482db2a0d4c03c2f067adaa3e921192f1e3c6
Loong <loong2460@gmail.com>
2025-11-19 16:31:56 +0800

    refactor(*): replace old Gleez Config classes with Kohana equivalents

---
+++ system/classes/Kohana/Config/File/Reader.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * File-based configuration reader. Multiple configuration directories can be
+ * used by attaching multiple instances of this class to [Kohana_Config].
+ *
+ * @package    Kohana
+ * @category   Configuration
+ * @author     Kohana Team
+ * @copyright  (c) 2009-2012 Kohana Team
+ * @license    https://kohana.top/license
+ */
+class Kohana_Config_File_Reader implements Kohana_Config_Reader
+{
+    /**
+     * The directory where config files are located
+     * @var string
+     */
+    protected $_directory = '';
+
+    /**
+     * Creates a new file reader using the given directory as a config source
+     *
+     * @param string $directory Configuration directory to search
+     */
+    public function __construct(string $directory = 'config')
+    {
+        // Set the configuration directory name
+        $this->_directory = trim($directory, '/');
+    }
+
+    /**
+     * Load and merge all the configuration files in this group.
+     *
+     *     $config->load($name);
+     *
+     * @param string $group Configuration group name
+     * @return  array
+     * @uses    Kohana::load
+     */
+    public function load(string $group): array
+    {
+        $config = [];
+
+        if ($files = Kohana::find_file($this->_directory, $group, null, true)) {
+            foreach ($files as $file) {
+                // Merge each file to the configuration array
+                $config = Arr::merge($config, Kohana::load($file));
+            }
+        }
+
+        return $config;
+    }
+
+}
