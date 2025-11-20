08d6dcb514d0d5d6b188c068f1796ed99f7f256c
Loong <loong2460@gmail.com>
2025-11-20 17:53:17 +0800

    refactor(gleez): remove legacy Controller and Response classes

--- modules/gleez/classes/request/exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package    Gleez\Request
- * @author     Gleez Team
- * @copyright  (c) 2011-2015 Gleez Technologies
- * @license    http://gleezcms.org/license  Gleez CMS License
- */
-class Request_Exception extends Kohana_Exception
-{}
