b77e9dfcc22e649ecf35dc6fa8296fc62c8beee6
Loong <loong2460@gmail.com>
2025-12-01 17:45:01 +0800

    refactor(gleez): update error display to show exception class name

--- modules/gleez/views/errors/stack.php
+++ modules/gleez/views/errors/stack.php
@@ -114,7 +114,7 @@ function koggle(elem)
 	    <br>
 
       <div id="kohana_error">
-	<h1><span class="type"><?php echo $type ?> [ <?php echo $code ?> ]:</span> <span class="message"><?php echo htmlspecialchars((string)$message, ENT_QUOTES, Gleez::$charset, true); ?></span></h1>
+	<h1><span class="type"><?php echo $class ?> [ <?php echo $code ?> ]:</span> <span class="message"><?php echo htmlspecialchars((string)$message, ENT_QUOTES, Gleez::$charset, true); ?></span></h1>
 	<div id="<?php echo $error_id ?>" class="content">
 		<p><span class="file"><?php echo Debug::path($file) ?> [ <?php echo $line ?> ]</span></p>
 		<?php echo Debug::source($file, $line) ?>
