d99d1d3ae25a1d2758fcc9810a129625fda477a7
Loong <loong2460@gmail.com>
2025-12-03 18:09:46 +0800

    refactor(*): simplify Cache class by extending Kohana_Cache

--- modules/gleez/config/cache.php
+++ modules/gleez/config/cache.php
@@ -1,70 +1,63 @@
 <?php
 
-return array(
-	'memcache' => array(
-		'driver'             => 'memcache',
-		'default_expire'     => 3600,
-		'compression'        => FALSE,              // Use Zlib compression (can cause issues with integers)
-		'servers'            => array(
-			array(
-				'host'             => 'localhost',  // Memcache Server
-				'port'             => 11211,        // Memcache port number
-				'persistent'       => FALSE,        // Persistent connection
-				'weight'           => 1,
-				'timeout'          => 1,
-				'retry_interval'   => 15,
-				'status'           => TRUE,
-			),
-		),
-		'instant_death'      => TRUE,               // Take server offline immediately on first fail (no retry)
-	),
-	'memcachetag' => array(
-		'driver'             => 'memcachetag',
-		'default_expire'     => 3600,
-		'compression'        => FALSE,              // Use Zlib compression (can cause issues with integers)
-		'servers'            => array(
-			array(
-				'host'             => 'localhost',  // Memcache Server
-				'port'             => 11211,        // Memcache port number
-				'persistent'       => FALSE,        // Persistent connection
-				'weight'           => 1,
-				'timeout'          => 1,
-				'retry_interval'   => 15,
-				'status'           => TRUE,
-			),
-		),
-		'instant_death'      => TRUE,
-	),
-	'apc'      => array(
-		'driver'             => 'apc',
-		'default_expire'     => 3600,
-	),
-	'wincache' => array(
-		'driver'             => 'wincache',
-		'default_expire'     => 3600,
-	),
-	'sqlite'   => array(
-		'driver'             => 'sqlite',
-		'default_expire'     => 3600,
-		'database'           => APPPATH.'cache/gleez-cache.sql3',
-		'schema'             => 'CREATE TABLE caches(id VARCHAR(500) PRIMARY KEY, tags VARCHAR(255), expiration INTEGER, cache TEXT)',
-		'index'              => 'CREATE UNIQUE INDEX [cache_unique] ON [caches] ([id])',
-	),
-	'eaccelerator'           => array(
-		'driver'             => 'eaccelerator',
-	),
-	'xcache'   => array(
-		'driver'             => 'xcache',
-		'default_expire'     => 3600,
-	),
-	'file'    => array(
-		'driver'             => 'file',
-		'cache_dir'          => APPPATH.'cache',
-		'default_expire'     => 3600,
-		'ignore_on_delete'   => array(
-			'.gitignore',
-			'.git',
-			'.svn'
-		)
-	),
-);
+return [
+//    'memcached' => [
+//        'driver' => 'memcached',
+//        'default_expire' => 3600,
+//        'servers' => [
+//            'local' => [
+//                // Memcached Server
+//                'host' => 'localhost',
+//                // Memcached port number
+//                'port' => 11211,
+//                'weight' => 1,
+//            ],
+//        ]
+//    ],
+//    'memcache' => [
+//        'driver' => 'memcache',
+//        'default_expire' => 3600,
+//        // Use Zlib compression (can cause issues with integers)
+//        'compression' => false,
+//        'servers' => [
+//            'local' => [
+//                // Memcache Server
+//                'host' => 'localhost',
+//                // Memcache port number
+//                'port' => 11211,
+//                // Persistent connection
+//                'persistent' => false,
+//                'weight' => 1,
+//                'timeout' => 1,
+//                'retry_interval' => 15,
+//                'status' => true,
+//            ],
+//        ],
+//        // Take server offline immediately on first fail (no retry)
+//        'instant_death' => true,
+//    ],
+//    'apcu' => [
+//        'driver' => 'apcu',
+//        'default_expire' => 3600,
+//    ],
+//    'wincache' => [
+//        'driver' => 'wincache',
+//        'default_expire' => 3600,
+//    ],
+//    'sqlite' => [
+//        'driver' => 'sqlite',
+//        'default_expire' => 3600,
+//        'database' => APPPATH . 'cache/kohana-cache.sql3',
+//        'schema' => 'CREATE TABLE caches(id VARCHAR(127) PRIMARY KEY, tags VARCHAR(255), expiration INTEGER, cache TEXT)',
+//    ],
+    'file' => [
+        'driver' => 'file',
+        'cache_dir' => APPPATH . 'cache',
+        'default_expire' => 3600,
+        'ignore_on_delete' => [
+            '.gitignore',
+            '.git',
+            '.svn'
+        ]
+    ]
+];
