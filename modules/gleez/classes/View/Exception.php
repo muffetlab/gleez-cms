7bc81fea32173ff06136b8925b2a96eaac28c6a2
Loong <loong2460@gmail.com>
2025-12-04 17:28:21 +0800

    refactor(gleez): remove unused View_Exception class

--- modules/gleez/classes/View/Exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @package    Gleez\View
- * @author     Gleez Team
- * @copyright  (c) 2011-2015 Gleez Technologies
- * @license    http://gleezcms.org/license  Gleez CMS License
- */
-class View_Exception extends Kohana_Exception
-{}
