38fdc121f64beba6f1a53a1560b4b762a4629e6c
Loong <loong2460@gmail.com>
2026-06-24 12:31:59 +0800

    style: rename dochead and docbody classes to kebab-case

--- themes/cerber/views/welcome.php
+++ themes/cerber/views/welcome.php
@@ -26,11 +26,11 @@
 		</ul>
 	</div>
 
-	<div class="dochead">
+    <div class="doc-head">
 		<p>We hate reinventing the wheel. But when the wheel doesn't exist, or is square, we're not afraid to invent a round one.</p>
 	</div>
 
-	<div class="docbody">
+    <div class="doc-body">
 		<h3>What Is Gleez CMS?</h3>
 		<p>Gleez is an open source, <a href="http://en.wikipedia.org/wiki/Content_management_system">extensible Content Management System</a> that is built on the <a href="http://kohanaframework.org">Kohana Framework</a>. Gleez is the fusion of many good concepts from the 4 most popular systems: Joomla, Drupal, Wordpress and Ruby Rails. With years of experience using these, we've gathered great ideas and in some cases, improved on them.</p>
 		<p>With an emphasis on security and functionality, Gleez is a professional and robust system suitable for any business or organization. Built on the PHP programming language and the MySQL database, Gleez delivers superb performance on any size application.</p>
