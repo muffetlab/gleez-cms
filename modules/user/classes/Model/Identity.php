b58e90188d0bf22359c9c14a12f6d754a48bf992
Loong <loong2460@gmail.com>
2026-07-14 15:18:14 +0800

    refactor(user): remove unused Identity methods

--- modules/user/classes/Model/Identity.php
+++ modules/user/classes/Model/Identity.php
@@ -25,25 +25,4 @@ class Model_Identity extends ORM {
 	protected $_belongs_to = array(
 		'user' => array('foreign_key' => 'user_id')
 	);
-
-    /**
-     * Validates that this identity is unique
-     *
-     * @param string URL to validate
-     * @return bool True if the URL is unique, false otherwise.
-     * @throws Kohana_Exception
-     */
-    public static function unique_identity($identity): bool
-    {
-		return (bool) DB::select(array(DB::expr('COUNT(provider)'), 'total'))
-			->from('identities')
-			->where('provider', '=', $identity)
-			->execute()
-			->get('total');
-	}
-
-	public function login()
-	{
-
-	}
 }
