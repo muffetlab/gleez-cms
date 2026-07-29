1a8851c8b3c3c503753dfd74a4fd7ea1f0d236a8
Loong <loong2460@gmail.com>
2026-07-29 17:58:03 +0800

    refactor(gleez): change user param type to Model_User

--- modules/gleez/classes/ACL.php
+++ modules/gleez/classes/ACL.php
@@ -285,13 +285,13 @@ class ACL {
      * If the user is not given, used currently active user
      *
      * @param string $perm_name Permission name
-     * @param ORM|null $user User object [Optional]
+     * @param Model_User|null $user User object [Optional]
      * @return  boolean
      * @throws Cache_Exception
      * @throws Kohana_Exception
      * @uses    User::active_user
      */
-    public static function check(string $perm_name, ORM $user = null): bool
+    public static function check(string $perm_name, Model_User $user = null): bool
     {
 		// If we weren't given an auth object
 		if (is_null($user))
