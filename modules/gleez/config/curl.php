319189acdf09c39aa0b9a5d098951627418398af
Loong <loong2460@gmail.com>
2026-08-20 15:27:14 +0800

    chore(gleez): remove unused curl config file

--- modules/gleez/config/curl.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    CURLOPT_USERAGENT => 'Mozilla/5.0 (compatible; Gleez v' . Gleez::VERSION . ' https://gleezcms.org/)',
-    CURLOPT_CONNECTTIMEOUT => 5,
-    CURLOPT_TIMEOUT => 5,
-    CURLOPT_HEADER => false,
-];
